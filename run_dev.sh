9370e2e55f5df94a1fde63fccece3b21eab9c4de
Karl Wulfert <pic16f877.ccs@emailn.de>
2026-05-09 22:05:22 +0200

    Refactor run_dev.sh to improve readability and maintainability

--- run_dev.sh
+++ run_dev.sh
@@ -1,183 +1,199 @@
 #!/bin/bash
 set -e
 
-EXTENSION='timeFromStart@pic16f877ccs.github.com'
-EXTENSIONNAME='Time from start (Uptime)'
+readonly _EXTENSION='timeFromStart@pic16f877ccs.github.com'
+readonly _EXTENSION_NAME='Time from start (Uptime)'
+readonly PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly BUILD_DIST="${PROJECT_ROOT}/build/dist/"
 
 first_arg="$1"
 second_arg="$2"
 
 build() {
-    mkdir -p './build/temp'
-    mkdir -p './build/dist'
+    local build_temp="${PROJECT_ROOT}/build/temp/"
+    local path_to_schema="${PROJECT_ROOT}/assets/org.gnome.shell.extensions.time-from-start.gschema.xml"
+    local path_to_src="${PROJECT_ROOT}/src/"
 
-    MULTIPATHS=('./assets/sounds' './src')
-    rm -rf './build/temp/*'
-    cp -r $(find './src' -mindepth 1 -maxdepth 1 -not -name 'assets') './build/temp/.'
-    cp -r './assets/sounds' './build/temp/.'
+    mkdir -p "$build_temp"
+    mkdir -p "$BUILD_DIST"
+
+    rm -rf "${build_temp:?}"/*
+
+find "$path_to_src" -mindepth 1 -maxdepth 1 -not -name 'assets' -exec cp -r {} "$build_temp" \;
+
+    cp -r "${PROJECT_ROOT}"/assets/sounds "$build_temp"
 
     echo 'Packing...'
 
-    EXTRASRCS=$(find './build/temp/' -mindepth 1 -maxdepth 1 ! -name "metadata.json" ! -name "extension.js" ! -name "prefs.js" ! -name "stylesheet.css")
+    mapfile -t extra_source_list < <(find "${build_temp}" -mindepth 1 -maxdepth 1 \
+        ! -name 'metadata.json' ! -name 'extension.js' ! -name 'prefs.js' ! -name 'stylesheet.css')
 
-    ESOURCES=()
-
-    for ELEM in $EXTRASRCS; do
-      ESOURCES+=("--extra-source=${PWD}/${ELEM}")
+    local extra_source
+    for extra_source in "${extra_source_list[@]}"; do
+        extra_sources+=("--extra-source=${extra_source}")
     done
 
-    SCHEMA='./../../assets/org.gnome.shell.extensions.time-from-start.gschema.xml'
-    PODIR='./../../assets/locale'
+    if gnome-extensions pack -f -o "$BUILD_DIST" \
+        --schema="$path_to_schema" \
+        "${extra_sources[@]}" \
+        --podir="$path_to_podir" \
+        "$build_temp"; then
 
-    if gnome-extensions pack -f -o './build/dist' --schema="$SCHEMA" "$ESOURCES" --podir="$PODIR" './build/temp'; then
         echo '...'
-        echo 'Success!'
+        echo 'Build successful.'
     fi
 }
 
 nested() {
-    if [ "$first_arg" = 'fullhd' ]; then
-        echo 'Full Hd screen size...'
+    local first_arg="${1}"
 
-        export MUTTER_DEBUG_DUMMY_MODE_SPECS=1920x1080 
-        export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=1.5 
+    echo '...'
+    export MUTTER_DEBUG_NUM_DUMMY_MONITORS=1 
+
+    if [ "$(gnome-shell --version | awk '{print int($3)}')" -ge 49 ]; then
+        dbus-run-session gnome-shell --devkit --wayland
     else
-        echo 'UHD screen size...'
-        export MUTTER_DEBUG_DUMMY_MODE_SPECS=3840x2100 
-        export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=2.0 
-        export MUTTER_DEBUG_NUM_DUMMY_MONITORS=1 
-    fi
+        if [ "$first_arg" = '--fullhd' ]; then
+            echo 'Full Hd screen size...'
+            echo '...' 
 
-    dbus-run-session -- gnome-shell --unsafe-mode --nested --wayland --no-x11
+            export MUTTER_DEBUG_DUMMY_MODE_SPECS=1920x1080 
+            export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=1.5 
+        else
+            echo 'UHD screen size...'
+            echo '...' 
+
+            export MUTTER_DEBUG_DUMMY_MODE_SPECS=3840x2100 
+            export MUTTER_DEBUG_DUMMY_MONITOR_SCALES=2.0 
+        fi
+
+        export MUTTER_DEBUG_NUM_DUMMY_MONITORS=1 
+        dbus-run-session -- gnome-shell --unsafe-mode --nested --wayland --no-x11
+    fi
 }
 
 debug() {
+    local fullhd="${1}"
     echo 'Debugging...'
     echo '...'
-    if gnome-extensions list | grep -Ewoq "$EXTENSION"; then
-        echo "The ${EXTENSION} is installed"
+
+    build
+    install
+
+    if gnome-extensions list | grep -Ewoq "$_EXTENSION"; then
+        echo "The ${_EXTENSION} is installed"
     else
-        echo "The ${EXTENSION} is not installed"
+        echo "The ${_EXTENSION} is not installed"
         exit 1
     fi
 
-    if gnome-extensions show "$extension" | grep -Ewoq 'INACTIVE'; then
+    if gnome-extensions show "$_EXTENSION" | grep -Ewoq 'INACTIVE'; then
         enable
-    #else
-    #    if ! [[ "$second_arg" == "--force" ]]; then
-    #        exit 1
-    #    fi
     fi
 
-    nested
+    nested "$fullhd"
 }
 
 install() {
-    if [[ "$second_arg" == '-b' ]]; then
+    local flag="${1}"
+    if [[ "$flag" == '-b' ]]; then
         build
         echo "..."
     fi
 
     echo 'Installing...'
-    gnome-extensions install --force "./build/dist/${EXTENSION}.shell-extension.zip"
+    gnome-extensions install --force "${BUILD_DIST}${_EXTENSION}.shell-extension.zip"
     echo '...'
     echo 'Success!'
 }
 
 uninstall() {
     echo 'Uninstalling...'
-    gnome-extensions uninstall "$EXTENSION"
+    gnome-extensions uninstall "$_EXTENSION"
     echo '...'
     echo 'Success!'
 }
 
 enable() {
     echo 'Enabling...'
-    gnome-extensions enable "$EXTENSION"
+    gnome-extensions enable "$_EXTENSION"
     echo '...'
     echo 'Success!'
 }
 
 disable() {
     echo 'Disabling...'
-    gnome-extensions disable "$EXTENSION"
+    gnome-extensions disable "$_EXTENSION"
     echo '...'
     echo 'Success!'
 }
 
+prefs() {
+    echo 'Opening prefs...'
+    gnome-extensions prefs "$_EXTENSION"
+}
+ 
+key() {
+    local first_arg="${1:?Error: key name required}"
+
+    echo 'Reading setting key...'
+    echo '...'
+
+    dconf read "/org/gnome/shell/extensions/${_EXTENSION_NAME}/$first_arg"
+}
+ 
+list() {
+    echo 'List all setting keys...'
+    echo '...'
+    dconf list "/org/gnome/shell/extensions/${_EXTENSION_NAME}/"
+}
+
 watch() {
-  echo 'Watching for setting changes...'
-  dconf watch "/org/gnome/shell/extensions/${EXTENSIONNAME}/"
+    echo 'Watching for setting changes...'
+    dconf watch "/org/gnome/shell/extensions/${_EXTENSION_NAME}/"
 }
 
 reset() {
-  echo 'Watching for setting changes...'
-  dconf reset -f "/org/gnome/shell/extensions/${EXTENSIONNAME}/"
-}
-
-prefs() {
-  echo 'Opening prefs...'
-  gnome-extensions prefs "$EXTENSION"
-}
-
-translations() {
-  echo "Updating translations..."
-
-  touch "assets/locale/${EXTENSION}.pot"
-
-  find ./src -type f -a -iname "*.js" | xargs xgettext --from-code=UTF-8 \
-    --add-comments \
-    --join-existing \
-    --keyword=_ \
-    --keyword=C_:1c,2 \
-    --language=Javascript \
-    --output="assets/locale/${EXTENSION}.pot"
-
-  for pofile in assets/locale/*.po; do
-    echo "Updating: $pofile"
-    msgmerge -U "$pofile" "assets/locale/${EXTENSION}.pot"
-  done
-
-  rm assets/locale/*.po~ 2>/dev/null
-  echo "Done"
+    echo 'Resetting all settings...'
+    dconf reset -f "/org/gnome/shell/extensions/${_EXTENSION_NAME}/"
 }
 
 case "$1" in
 debug)
-  debug
-  ;;
-install)
-  install
-  ;;
-uninstall)
-  uninstall
-  ;;
-enable)
-  enable
-  ;;
-disable)
-  disable
-  ;;
+    debug "$2"
+    ;;
 build)
-  build
-  ;;
-# release)
-#   build "release"
-#   ;;
-translations)
-  translations
-  ;;
+    build
+    ;;
+install)
+    install "$2"
+    ;;
+uninstall)
+    uninstall
+    ;;
+enable)
+    enable
+    ;;
+disable)
+    disable
+    ;;
 prefs)
-  prefs
-  ;;
+    prefs
+    ;;
+key)
+     key "$2"
+     ;;
+list)
+     list
+     ;;
 watch)
-  watch
-  ;;
+    watch
+    ;;
 reset)
-  reset
-  ;;
+    reset
+    ;;
 *)
-  echo "Usage: $0 {debug|build|install|uninstall|enable|disable|prefs|watch|reset|translations}"
-  exit 1
-  ;;
+    echo "Usage: $0 {debug|build|install|uninstall|enable|disable|prefs|key|list|watch|reset}"
+    exit 1
+    ;;
 esac
